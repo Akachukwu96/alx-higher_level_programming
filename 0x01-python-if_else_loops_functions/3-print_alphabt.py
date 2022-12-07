aebe72c9d9a34071630278f6913f3fc2edc8f790
Akachukwu <105462943+Akachukwu96@users.noreply.github.com>
2022-12-07 12:27:53 +0100

    Update 3-print_alphabt.py
    task

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,1 +1,6 @@
-f
+#!/usr/bin/python3
+
+"""Print the alphabet in lowercase except q and e."""
+for i in range(97, 123):
+    if chr(i) is not 'q' and chr(i) is not 'e':
+        print("{}".format(chr(i), end="")
