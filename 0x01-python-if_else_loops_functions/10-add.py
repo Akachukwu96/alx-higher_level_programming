4efe9701fad51beac17555b74c5c7b5f764cbd3a
Akachukwu <105462943+Akachukwu96@users.noreply.github.com>
2023-01-07 17:41:32 +0100

    Update 10-add.py
    update

--- 0x01-python-if_else_loops_functions/10-add.py
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -3,3 +3,5 @@ def add(a, b):
     """  A function that adds two integers and returns the result """
     sum = a + b
     return (sum)
+# Easier way
+# return (a+b), without creating rhe variable sum
