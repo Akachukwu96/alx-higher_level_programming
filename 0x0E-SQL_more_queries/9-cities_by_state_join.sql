a3a59de37d4812a6e5708b1069b54ed5bf737418
Akachukwu <105462943+Akachukwu96@users.noreply.github.com>
2023-02-21 08:30:41 +0100

    state

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -3,3 +3,8 @@ SELECT cities.id, cities.name, states.name
 FROM cities
 INNER JOIN states ON cities.state_id=states.id
 ORDER BY cities.id ASC;
+
+or
+
+SELECT cities.id, cities.name, states.name FROM cities LEFT JOIN states ON states.id = cities.state_id ORDER BY cities.id;
+
