d3918b9b9c762dce4d179540ed2ff35968b3f02b
Akachukwu <105462943+Akachukwu96@users.noreply.github.com>
2023-03-15 08:24:18 +0100

    multiLang

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/*
+   A JS Script that prints the elements of an array in a single line.
+*/
+const multiLang = ['c is fun', 'Python is cool', 'JavaSript is amazing'];
+for (const item of multiLang) {
+  console.log(item);
+}
